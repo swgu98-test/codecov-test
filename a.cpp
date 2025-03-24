64f9aef835a68939d4c772b9f74714b0496e1bda
gushiwei <gushiwei@baidu.com>
2025-03-24 14:01:45 +0000

    Init

---
+++ a.cpp
@@ -0,0 +1,6 @@
+#include "a.h"
+
+int my_add(int a, int b) {
+  return a + b;
+}
+
---
+++ test_my_add.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "a.h"
+
+using namespace std;
+
+int main()
+{
+  auto c = my_add(1, 2);
+  cout << "1 + 2 = " << c << endl;
+  return 0;
+}
